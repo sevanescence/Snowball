036510577a4da0e32935b6242cc9c9baf07adcab
Makoto Miyamoto <48812892+MakotoMiyamoto@users.noreply.github.com>
2019-06-29 15:18:26 -0500

    Add files via upload

--- Snowball/src/main/java/com/makotomiyamoto/snowball/object/MemberUser.java
+++ Snowball/src/main/java/com/makotomiyamoto/snowball/object/MemberUser.java
@@ -5,12 +5,23 @@ import net.dv8tion.jda.api.entities.User;
 public class MemberUser {
 
     private String memberUuid;
-    private int messages, level;
+    private int messages, level, warns, strikes, kicks;
+    private String[] bans;
 
     public MemberUser(String memberUuid, int msgs, int lvl) {
         this.memberUuid = memberUuid;
         this.messages = msgs;
         this.level = lvl;
+    }
+
+    public MemberUser(String memberUuid, int msgs, int lvl, int warns, int strikes, int kicks, String[] bans) {
+        this.memberUuid = memberUuid;
+        this.messages = msgs;
+        this.level = lvl;
+        this.warns = warns;
+        this.strikes = strikes;
+        this.kicks = kicks;
+        this.bans = bans;
     }
 
     public MemberUser(User user) {
@@ -54,6 +65,42 @@ public class MemberUser {
                 System.out.println("User is level " + this.level);
                 break;
         }
+    }
+    public int getWarns() {
+        return warns;
+    }
+    public int getStrikes() {
+        return strikes;
+    }
+    public void setWarns(int warns) {
+        this.warns = warns;
+    }
+    public void setStrikes(int strikes) {
+        this.strikes = strikes;
+    }
+    public int getKicks() {
+        return kicks;
+    }
+    public void setKicks(int kicks) {
+        this.kicks = kicks;
+    }
+    public String[] getBans() {
+        return bans;
+    }
+    public void setBans(String[] bans) {
+        this.bans = bans;
+    }
+    @SuppressWarnings("all")
+    public String getBansToString() {
+        if (bans.length < 1) {
+            return "`no bans :3`";
+        }
+        String message = " ```";
+        for (int i = 0; i < bans.length; i++) {
+            message += " " + (i+1) + ". " + bans[i] + "\n";
+        }
+        message += "```";
+        return message;
     }
 
 }
