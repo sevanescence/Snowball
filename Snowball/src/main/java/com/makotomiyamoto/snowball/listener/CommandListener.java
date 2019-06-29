f52655f17ec1f914abda6a15a5888d94cf8c8ca6
Makoto Miyamoto <48812892+MakotoMiyamoto@users.noreply.github.com>
2019-06-29 03:21:52 -0500

    Command prompt update
    Command prompt key is now '!' instead of '>'

--- Snowball/src/main/java/com/makotomiyamoto/snowball/listener/CommandListener.java
+++ Snowball/src/main/java/com/makotomiyamoto/snowball/listener/CommandListener.java
@@ -10,7 +10,7 @@ public final class CommandListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
 
-        if (!event.getMessage().getContentRaw().startsWith(">")) {
+        if (!event.getMessage().getContentRaw().startsWith("!")) {
             return;
         }
 
